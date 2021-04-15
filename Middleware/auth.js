b350275eca1f3b7323790e31b24f0142528f1153
NacereddineRebouh <llbl2153@gmail.com>
2021-04-15 11:36:39 +0100

    2

--- Middleware/auth.js
+++ Middleware/auth.js
@@ -2,22 +2,24 @@ const jwt = require("jsonwebtoken");
 const config = require("config");
 
 module.exports = function (req, res, next) {
-  console.log("auth middleware");
   //next is a callback
   //get token from the header
+  console.log("0.1");
   const token = req.header("x-auth-token");
-
+  console.log("0.2");
   //check token
   if (!token) {
     return res.status(401).json({ msg: "No Token, authrization denied" });
   }
-
+  // res.json({ Token: token });
   //verify token and retrieve user
   try {
     const decoded = jwt.verify(token, config.get("jwtSecret"));
 
     req.user = decoded.user;
+    console.log("0.2");
     next();
+    console.log("0.3");
   } catch (error) {
     res.status(401).json({ msg: "Token is not valid" });
   }
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,8 +1,26 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const auth = require("../../Middleware/auth");
+const Profile = require("../../models/Profile");
+// @route     GET api/profile/me
 
-// @route     GET api/profile
-
-router.get('/', (req, res) =>res.send('PROFILE ROUTE is here!'));
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate("user", ["name", "avatar"]);
+    console.log("1");
+    if (!profile) {
+      return res
+        .status(400)
+        .json({ msg: "There is no profile for this user !" });
+    }
+    console.log("2");
+    res.json(profile);
+  } catch (error) {
+    console.error(error.massage);
+    res.status(500).send("server error");
+  }
+});
 
 module.exports = router;
